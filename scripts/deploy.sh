eedfa9128bfe13612489b49d3944df5235849514
Marek Bejda <marek.bejda@ibm.com>
2020-07-13 16:00:26 -0400

    wrong api

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ invocation_error=0
 if [ -z ${APIKEY+x} ]; then echo 'Error: Environment variable APIKEY is undefined.'; invocation_error=1; fi
 # set optional Cloud Foundry variables if they are not set
 # - CF_API: IBM Cloud API endpoint (default to US-South region)
-if [ -z ${CF_API+x} ]; then export CF_API='https://api.us-south.cf.cloud.ibm.com'; fi
+if [ -z ${CF_API+x} ]; then export CF_API='https://api.ng.bluemix.net'; fi
 
 if [ ${invocation_error} -eq 1 ]; then echo 'Something went wrong, check for previous errors.'; exit 1; fi
 
